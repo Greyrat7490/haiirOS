7396a2561786e659cfdc97e2865ccdf83256f8df
Greyrat7490 <kevinschaperjahn13@gmail.com>
2023-03-05 11:47:23 +0100

    use XHCI as default (more typical) * you most likly boot from USB (so use XHCI) * added test-ahci make option

--- src/main.c
+++ src/main.c
@@ -39,29 +39,31 @@ void kernel_main(bloader_boot_info_t* boot_info) {
     init_ahci();
 
     ahci_dev_t* ahci_dev = get_ahci_dev(0, 0);
-                                // test reading more sectors than available
-    char* buffer = pmm_alloc((ahci_dev->sector_count*AHCI_SECTOR_SIZE+1) / FRAME_SIZE + 1);
-    uint64_t res = ahci_read(0, ahci_dev->sector_count*AHCI_SECTOR_SIZE+1, buffer, ahci_dev);
-    kprintln("read: '%s'", buffer+3);
-    kprintln("read: %x bytes", res);
+    if (ahci_dev != 0x0) {
+                                    // test reading more sectors than available (qemu)
+        char* buffer = pmm_alloc((0x800000 + AHCI_SECTOR_SIZE -1) / FRAME_SIZE + 1);
+        uint64_t res = ahci_read(0, 0x800000 + AHCI_SECTOR_SIZE, buffer, ahci_dev);
+        kprintln("read: '%s'", buffer+3);
+        kprintln("read: %x bytes", res);
 
-    /*
-     * WARNING DO NOT TEST ON YOUR REAL DEVICE OR YOUR BOOT SECTOR GETS OVERWRITEN
-     */
+        /*
+         * WARNING DO NOT TEST ON YOUR REAL DEVICE OR YOUR BOOT SECTOR GETS OVERWRITEN
+         */
 #ifdef WRITE_TEST
-    kprintln("WARNING DO NOT TEST ON YOUR REAL DEVICE OR YOUR BOOT SECTOR GETS OVERWRITEN");
+        kprintln("WARNING DO NOT TEST ON YOUR REAL DEVICE OR YOUR BOOT SECTOR GETS OVERWRITEN");
 
-    const char* msg = "   test message";
-    kprintln("write: '%s'", msg);
-    res = ahci_write(0, AHCI_SECTOR_SIZE, (void*)msg, ahci_dev);
-    kprintln("wrote: %d bytes", res);
+        const char* msg = "   test message";
+        kprintln("write: '%s'", msg);
+        res = ahci_write(0, AHCI_SECTOR_SIZE, (void*)msg, ahci_dev);
+        kprintln("wrote: %d bytes", res);
 
-    res = ahci_read(0, AHCI_SECTOR_SIZE, buffer, ahci_dev);
-    kprintln("read: '%s'", buffer+3);
-    kprintln("read: %d bytes", res);
+        res = ahci_read(0, AHCI_SECTOR_SIZE, buffer, ahci_dev);
+        kprintln("read: '%s'", buffer+3);
+        kprintln("read: %d bytes", res);
 #endif
 
-    while(1) __asm__("hlt");
+        while(1) __asm__("hlt");
+    }
     // --------------------------------------------------
 
     // test vbe -----------------------------------------
